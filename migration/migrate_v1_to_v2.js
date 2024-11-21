8511d28f0347620e52547a8bd3f8380378820654
jakjus <fazzebo@gmail.com>
2024-11-21 18:00:28 +0900

    migration cp

---
+++ migration/migrate_v1_to_v2.js
@@ -0,0 +1,27 @@
+const { db } = require('../db')
+const fs = require('fs');
+const { uid } = require("uid/secure");
+
+(async () => {
+	await fs.copyFileSync('./db.sqlite', './db.sqlite.bkp')
+	for await (const [guildId, data] of db.iterator()) {
+		console.log('\n\n===== DATA ======')
+		console.log(data)
+		console.log('\n\n===== BETS ======')
+		data.players.forEach(player => {
+			if (player.bets.length == 0) {
+				return
+			}
+			console.log('pb before', JSON.stringify(player.bets, null, 2))
+			const newBets = []
+			for (let b of player.bets) {
+				const amount = b.amount
+				delete b['amount']
+				b.uid = uid()
+				newBets.push([{uid: uid(), combination: [b], amount: amount}])
+			}
+			player.bets = newBets
+			console.log('pb after', JSON.stringify(player.bets, null, 2))
+		})
+	}
+})()
